a9cadb633155bdb7a584575d3ae43b0aeb7121bc
kaestro <didme654@snu.ac.kr>
2024-02-08 08:29:42 +0900

    MatrixScore의 개선 버전
    하고 있는 연산이, 실제로는 필요없는 것이 아닌지 점검해 볼 필요가 있다.

--- warm_ups/2024-Feb-Week2nd/LeetCode - 861.py
+++ warm_ups/2024-Feb-Week2nd/LeetCode - 861.py
@@ -27,3 +27,22 @@ class Solution:
             return sum(row[i] * (2 ** (col_len - i - 1)) for i in range(col_len))
 
         return sum(to_binary_number(grid[i]) for i in range(row_len))
+
+
+    # 1. 실제로 수를 다 만든 다음에 더할 필요가 없다는 사실에 유의해야 한다.
+    # 2. 맨 앞의 열은 이미 1이므로 그냥 row의 갯수에 2^(col_len - 1)을 곱해주면 된다.
+    # 3. 그 다음 열부터는 0의 갯수와 1의 갯수를 비교해서 더 큰 것을 선택한 뒤, 열의
+    #   위치에 따라 2의 거듭제곱을 곱해주면 된다.
+    def better_matrixScore(self, grid: List[List[int]]) -> int:
+        row_len = len(grid)
+        col_len = len(grid[0])
+        for i in range(row_len):
+            if grid[i][0] == 0:
+                for j in range(col_len):
+                    grid[i][j] ^= 1
+
+        res = (1 << (col_len - 1)) * row_len
+        for j in range(1, col_len):
+            one_cnt = sum(grid[i][j] == 1 for i in range(row_len))
+            res += max(one_cnt, row_len - one_cnt) * (1 << (col_len - j - 1))
+        return res
