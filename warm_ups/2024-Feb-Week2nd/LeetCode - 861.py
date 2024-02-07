f2c611d140c12a442ed0b84f48f3b7f6bec1195c
kaestro <didme654@snu.ac.kr>
2024-02-07 16:12:54 +0900

    First Solution - improvement needed
    첫 col value가 0인 경우만 뒤집고, column을 2번째부터 뒤집어대면 정답이
    나온다.
    
    현재 퍼포먼스가 좋지 않으므로, 이 상태에서 개선 지점 및 더 좋은
    알고리즘이 있는지에 대해 검증해볼 필요 있음

--- warm_ups/2024-Feb-Week2nd/LeetCode - 861.py
+++ warm_ups/2024-Feb-Week2nd/LeetCode - 861.py
@@ -6,26 +6,24 @@ class Solution:
     def matrixScore(self, grid: List[List[int]]) -> int:
         row_len = len(grid)
         col_len = len(grid[0])
-        while True:
+        for i in range(row_len):
+            if grid[i][0] == 0:
+                for j in range(col_len):
+                    grid[i][j] ^= 1
+
+        flipped = True
+        while flipped:
             flipped = False
-            for row in grid:
-                zero_cnt = row.count(0)
-                if zero_cnt > col_len // 2:
-                    flipped = True
-                    for i in range(col_len):
-                        row[i] ^= 1
-            for j in range(col_len):
+            for j in range(1, col_len):
                 zero_cnt = sum(grid[i][j] == 0 for i in range(row_len))
                 if zero_cnt > row_len // 2:
                     flipped = True
                     for i in range(row_len):
                         grid[i][j] ^= 1
-            if not flipped:
-                break
         
         # interpret the row of grid as binary number and
         # convert it to decimal number
         def to_binary_number(row):
             return sum(row[i] * (2 ** (col_len - i - 1)) for i in range(col_len))
 
-        return -1
+        return sum(to_binary_number(grid[i]) for i in range(row_len))
