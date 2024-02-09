60a4dda7f4ad723b6463f1841e05e0f94133eb43
kaestro <didme654@snu.ac.kr>
2024-02-09 15:28:21 +0900

    Improved beauty_sum

--- random_practices/February/Week2nd/LeetCode - 1781.py
+++ random_practices/February/Week2nd/LeetCode - 1781.py
@@ -7,6 +7,16 @@
 # space complexity: O(n^2)
 # proof: n(n+1)/2 substrings and each substring has a length of n
 
+# Question
+# For the highest burden is on creating all the substrings, which is O(n^2)
+# this is of bottleneck for the solution.
+# Is there any way to reduce the time complexity of the solution?
+
+# Answer
+# Yes, we can reduce the time complexity of the solution by 'not creating all the substrings'.
+# We can calculate the beauty of the substring while creating the substring.
+# This improves the time complexity of the solution from O(n^3) to O(n^2)
+
 from collections import Counter
 
 class Solution:
@@ -24,7 +34,15 @@ class Solution:
         # create a dictionary to store the frequency of each character
         frequency = Counter(substring)
         return max(frequency.values()) - min(frequency.values())
-
+    
+    def advanced_beautySum(self, input_str: str) -> int:
+        total_beauty = 0
+        for i in range(len(input_str)):
+            frequency = Counter()
+            for j in range(i, len(input_str)):
+                frequency[input_str[j]] += 1
+                total_beauty += max(frequency.values()) - min(frequency.values())
+        return total_beauty        
 
 def main():
     result = Solution().beautySum("aabcbaa")
