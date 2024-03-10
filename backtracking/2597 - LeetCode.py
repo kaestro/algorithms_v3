7d93d121779a27751af49a9a08049bca445ab94b
kaestro <didme654@snu.ac.kr>
2024-03-10 09:22:07 +0900

    [To Review] 일단 통과하는 해결방법 나옴!

--- backtracking/2597 - LeetCode.py
+++ backtracking/2597 - LeetCode.py
@@ -1,5 +1,6 @@
 # https://leetcode.com/problems/the-number-of-beautiful-subsets/description/
-from typing import List, Set
+from typing import List
+from collections import defaultdict
 
 
 class Solution:
@@ -16,22 +17,25 @@ class Solution:
     # 이 때 저장하는 자료구조는 list가 아닌 set을 사용하면 순회가 아니라 O(1)의
     # 시간복잡도로 찾을 수 있다.
 
+    # 궁금한점 => subset을 copy하는 것 자체가 문제 아닐까? copy하지 않으려면 dictionary를 사용하면 되지 않을까?
+    # countedSubsets에서 set을 찾는 것 자체가 O(n)의 시간복잡도를 가지는 것도 문제네. 이것도 dictionary로 바꿔야겠네.
+
     # BackTrack의 순회 자체의 시간복잡도는 O(2^n)이다. => 감당 가능?
+    # => backtrack이라서 시간복잡도는 O(2^n)이 아니다.
     def beautifulSubsets(self, nums: List[int], target_diff: int) -> int:
-        countedSubsets = []
-        def backTrack(nums: List[int], idx: int, subset: Set[int], target_diff: int) -> int:
+        def backTrack(nums: List[int], idx: int, num_cnt: defaultdict[int], target_diff: int) -> int:
             if idx == len(nums):
                 # 주의: list내에서 set은 in으로 찾을 수 없다.
-                if len(subset) == 0 or str(subset) in countedSubsets:
+                if len(num_cnt) == 0:
                     return 0
                 else:
-                    countedSubsets.append(str(subset))
                     return 1
 
-            cnt = backTrack(nums, idx + 1, subset.copy(), target_diff)
-            if nums[idx] - target_diff not in subset and nums[idx] + target_diff not in subset:
-                subset.add(nums[idx])
-                cnt += backTrack(nums, idx + 1, subset, target_diff)
+            cnt = backTrack(nums, idx + 1, num_cnt.copy(), target_diff)
+            if num_cnt[target_diff + nums[idx]] == 0 and num_cnt[nums[idx] - target_diff] == 0:
+                num_cnt[nums[idx]] += 1
+                cnt += backTrack(nums, idx + 1, num_cnt, target_diff)
+                num_cnt[nums[idx]] -= 1
             return cnt
         
-        return backTrack(nums, 0, set(), target_diff)
+        return backTrack(nums, 0, defaultdict(int), target_diff)
