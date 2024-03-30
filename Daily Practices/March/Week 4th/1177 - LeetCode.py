d3a4fbeb4c0c86ada9266748c5b848a7b8b0782f
kaestro <didme654@snu.ac.kr>
2024-03-30 09:24:36 +0900

    [Must Review] 26 bit binary num으로 dictionary 대체
    어차피 고정적인 26개의 integer가 홀짝만 구분할 필요가 있다면 list도
    필요없다. binary integer면 충분하다

--- Daily Practices/March/Week 4th/1177 - LeetCode.py
+++ Daily Practices/March/Week 4th/1177 - LeetCode.py
@@ -1,29 +1,30 @@
 # https://leetcode.com/problems/can-make-palindrome-from-substring/description/
+# https://leetcode.com/problems/can-make-palindrome-from-substring/solutions/371999/python-100-runtime-and-memory/
 
 from typing import List
-from collections import defaultdict
-from collections import Counter
 
 class Solution:
     # query = [left, right, k]
     # [inputs_str[left:right+1]]에 대해 k번 이하로 문자를 임의의 lowercase English letter로 변경하여
     # 해당 substring이 palindrome이 되도록 할 수 있는지 여부를 반환하는 문제
-
-    # rearrange에 대해 생각하지 못함
     def canMakePaliQueries(self, input_str: str, queries: List[List[int]]) -> List[bool]:
 
         N = 26
-        a_int = ord('a')
-        dp = [[0] * N]
-        for i in range(1, len(input_str) + 1):
-            dp.append(dp[i-1][:])
-            dp[i][ord(input_str[i-1]) - a_int] ^= 1
+        str_len = len(input_str)
+        str_to_int = list(map(lambda x: ord(x) - ord('a'), input_str))
 
-        result = []
-        for left, right, k in queries:
-            result.append(sum((dp[right+1][i] ^ dp[left][i]) for i in range(N) ) // 2 <= k)
+        dp = [0] * (str_len + 1)
+        for i in range(1, str_len + 1):
+            # dp[i]를 26bit의 binary로 표현한다. 각 bit는 해당 문자가 홀수번 등장했는지 짝수번 등장했는지를 나타낸다.
+            dp[i] = dp[i - 1] ^ (1 << str_to_int[i - 1])
+        
+        # binary로 표현한 수의 1의 개수를 세는 함수
+        ones_count = lambda x: bin(x).count('1')
 
-        return result
+        return [
+            ones_count(dp[right + 1] ^ dp[left]) >> 1 <= k
+            for left, right, k in queries
+        ]
 
 
 if __name__ == "__main__":
