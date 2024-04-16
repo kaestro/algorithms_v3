f5c5091eaa5f8b6cf833ccd6dccebb4b61f30cf6
kaestro <didme654@snu.ac.kr>
2024-04-16 09:09:38 +0900

    [Improved Idea to be implemented and to be proven]

--- Daily Practices/April/Week 3rd/1846[TODO] - LeetCode.py
+++ Daily Practices/April/Week 3rd/1846[TODO] - LeetCode.py
@@ -20,7 +20,8 @@ class Solution:
     # Then the problem is, the gaps between the elements are not 1, which can't be removed by rearranging the elements.
 
     # We also need to consider that the elements can be either increased or decreased.
-    # => 짝수개의 연속한 숫자들이 짝수개의 pair로 만들어지면 삭제 가능
+    # => 연속한 숫자들이 짝수개의 pair로 만들어지면 gap이 생기고, 홀수개의 pair로 만들어지면 1개씩 있는것과 동일하다.
+    # => 단독으로 있는 경우만 따로 처리해주면 된다. => 얘를 이용해서 gap을 채워주자.
     
     # => gap이 있을 때는 그거 위에서 중복된 것으로 메꿔주고, 남는 것들에 대해서 생각하면 됨?
     def maximumElementAfterDecrementingAndRearranging(self, arr: List[int]) -> int:
@@ -32,6 +33,10 @@ class Solution:
         # step 2: gap을 찾는다.
         soreted_set_arr = sorted(list(set(arr)))
         self.findGap(soreted_set_arr)
+
+        # step 3: gap을 메꾼다.
+
+        # step 4: 남은 수의 갯수가 정답이다.
 
         return result
     
