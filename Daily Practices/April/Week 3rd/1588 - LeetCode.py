cd8f5cd113d3a4f802efe154e84989e8746f9ab9
kaestro <didme654@snu.ac.kr>
2024-04-19 08:52:25 +0900

    [Must Review] Improved solution using counting

--- Daily Practices/April/Week 3rd/1588 - LeetCode.py
+++ Daily Practices/April/Week 3rd/1588 - LeetCode.py
@@ -22,3 +22,13 @@ class Solution:
             prefix_sum[i + 1] = prefix_sum[i] + arr[i]
         return prefix_sum
     
+    # https://leetcode.com/problems/sum-of-all-odd-length-subarrays/solutions/854184/java-c-python-o-n-time-o-1-space/?source=submission-ac
+    # O(n) time complexity, O(1) space complexity
+    # arr[k] 원소가 포함된 subarray의 개수는 (k + 1) * (n - k)이다.
+    # 이 중 홀수의 개수는 {(k + 1) * (n - k) + 1} // 2이다.
+    # 배열의 길이가 홀/짝의 갯수는 홀수만 유일하게 길이가 홀수 일 때 1개 많을 수 있다.
+    def betterSumOddLengthSubarrays(self, arr: List[int]) -> int:
+        result = 0
+        for i in range(len(arr)):
+            result += ((i + 1) * (len(arr) - i) + 1) // 2 * arr[i]
+        return result
