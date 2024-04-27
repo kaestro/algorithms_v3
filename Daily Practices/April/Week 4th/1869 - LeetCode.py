0ad2579f8b8c4c2218b5fe4c8b675fa50330290d
kaestro <didme654@snu.ac.kr>
2024-04-27 09:12:13 +0900

    [MUST Review commit delayed] This is the one

--- Daily Practices/April/Week 4th/1869 - LeetCode.py
+++ Daily Practices/April/Week 4th/1869 - LeetCode.py
@@ -3,9 +3,17 @@
 class Solution:
     def checkZeroOnes(self, inputString: str) -> bool:
         lenLongestOne, lenLongestZero = 0, 0
+        lenCurrentOne, lenCurrentZero = 0, 0
 
-        prev = None
-        for 
+        for i in range(len(inputString)):
+            if inputString[i] == '1':
+                lenCurrentOne += 1
+                lenCurrentZero = 0
+            else:
+                lenCurrentZero += 1
+                lenCurrentOne = 0
 
-        result = False
-        return result
+            lenLongestOne = max(lenLongestOne, lenCurrentOne)
+            lenLongestZero = max(lenLongestZero, lenCurrentZero)
+
+        return lenLongestOne > lenLongestZero
