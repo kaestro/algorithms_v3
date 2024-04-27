a9f1f9ce0fc700ee1f264ab234f68c0c76436637
kaestro <didme654@snu.ac.kr>
2024-04-27 09:11:01 +0900

    [Must Review] How to count consecutive characters

---
+++ Daily Practices/April/Week 4th/1869 - LeetCode.py
@@ -0,0 +1,11 @@
+# https://leetcode.com/problems/longer-contiguous-segments-of-ones-than-zeros/description/
+
+class Solution:
+    def checkZeroOnes(self, inputString: str) -> bool:
+        lenLongestOne, lenLongestZero = 0, 0
+
+        prev = None
+        for 
+
+        result = False
+        return result
