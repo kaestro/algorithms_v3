a1d307306c0bdb3360309bd7de03f957079dcc02
kaestro <didme654@snu.ac.kr>
2024-05-05 10:54:26 +0900

    Changed 2996 to Todo

---
+++ Daily Practices/May/Week 2nd/[To Do]2996 - LeetCode.py
@@ -0,0 +1,6 @@
+# https://leetcode.com/problems/smallest-missing-integer-greater-than-sequential-prefix-sum/description/
+from typing import List
+
+class Solution:
+    def missingInteger(self, nums: List[int]) -> int:
+        return -1
