da18a5a99c2b1515e96b9f318c1ec6a8b25b160d
kaestro <didme654@snu.ac.kr>
2024-05-15 11:10:17 +0900

    Delayed updates

--- Daily_Practices/May/Week_3rd/232 - LeetCode[No Review].ts
+++ Daily_Practices/May/Week_3rd/232 - LeetCode[No Review].ts
@@ -1,8 +1,12 @@
 //https://leetcode.com/problems/power-of-two/description/
 
 function isPowerOfTwo(n: number): boolean {
+    if (n < 1) return false;
+
     while (n > 1) {
         if (n % 2 !== 0) return false;
         n = n / 2;
     }
+
+    return true;
 };
--- Daily_Practices/May/Week_3rd/345 - LeetCoe[To Review].ts
+++ Daily_Practices/May/Week_3rd/345 - LeetCoe[To Review].ts
@@ -4,6 +4,20 @@ function reverseVowels(sentence: string): string {
     let start = 0, end = sentence.length - 1;
     const vowels = ['a', 'e', 'i', 'o', 'u'];
     let sentenceArray = sentence.split('');
+
+    while (start < end) {
+        if (vowels.includes(sentenceArray[start].toLowerCase()) && vowels.includes(sentenceArray[end].toLowerCase())) {
+            swap(sentenceArray, start, end);
+            start++;
+            end--;
+        } else if (vowels.includes(sentenceArray[start].toLowerCase())) {
+            end--;
+        } else {
+            start++;
+        }
+    }
+
+    return sentenceArray.join('');
 };
 
 
