70d9a4d2360071ba76f0ba4eb4de8c5509f6dd5f
kaestro <didme654@snu.ac.kr>
2024-09-29 10:57:07 +0900

    Easy problem

---
+++ Daily_Practices/September/leetcode - 908.cpp
@@ -0,0 +1,15 @@
+// https://leetcode.com/problems/smallest-range-i/description/
+#include <vector>
+
+class Solution {
+public:
+    int smallestRangeI(std::vector<int>& nums, int k) {
+        if (nums.size() == 1) return 0;
+
+        int min = *std::min_element(nums.begin(), nums.end());
+        int max = *std::max_element(nums.begin(), nums.end());
+
+        if (max - min <= 2 * k) return 0;
+        return max - min - 2 * k;
+    }
+};
